e221d56538a0e7936193787c24c775866803535e
Cleberw3b <cleberw3b@gmail.com>
2017-02-19 05:07:17 -0300

    update

--- src/main/java/com/ipayso/util/validators/EmailValidator.java
+++ src/main/java/com/ipayso/util/validators/EmailValidator.java
@@ -1,5 +1,8 @@
 package com.ipayso.util.validators;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.validation.ConstraintValidator;
@@ -13,6 +16,11 @@ import javax.validation.ConstraintValidatorContext;
  * @see ValidEmail
  */
 public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
+	
+	private Pattern pattern;
+    private Matcher matcher;
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    											
 	
 	/**
 	 * Create an Annotation to ValidEmail 
@@ -39,15 +47,18 @@ public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
 	 * @throws AddressException
 	 */
 	private boolean validateEmail(String email) {
-		boolean isValid = false;
-		try {
+		//boolean isValid = false;
+		pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+		/*try {
 			InternetAddress internetAddress = new InternetAddress(email);
 			internetAddress.validate();
 			isValid = true;
 		} catch (AddressException e) {
 			System.out.println("You are in catch block -- Exception Occurred for: " + email);
 		}
-		return isValid;
+		return isValid;*/
 
 	}
 }
